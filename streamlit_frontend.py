2803170475b323874b2787175bbf24d24a2f3b2c
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-08-29 17:13:44 +0530

    initial commit

---
+++ streamlit_frontend.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage 
+
+
+CONFIG={'configurable': {'thread_id': 'thread-1'}}
+
+#st.session_state : it is a dictionary ,whose content reamin as it is even after re-running of whole script
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+user_input=st.chat_input('Type Here')
+
+if user_input:
+
+    #first add message to message history
+    st.session_state['message_history'].append({'role':'user', 'content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response=chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    ai_message=response['messages'][-1].content
+
+
+    st.session_state['message_history'].append({'role':'assistant', 'content':user_input})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
+
+    
